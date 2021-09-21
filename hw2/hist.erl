d9c1ffdd0539d95033b80a5d13e48b8fccf22a21
Flaxen <alexander.carlsson@live.se>
2021-09-21 11:34:06 +0200

    added functions for history module

---
+++ hw2/hist.erl
@@ -0,0 +1,18 @@
+-module(hist).
+-export([new/1, update/3]).
+
+new(Name) ->
+  [{Name, inf}].
+
+update(Node, N, History) ->
+  Tuple = lists:keyfind(Node, 1, History),
+  case Tuple of
+    false -> [{Node, N}|History];
+    Tuple -> {_, N2} = Tuple,
+    if N2 < N ->
+      Updated = lists:keyreplace(Node, 1, History, {Node, N}),
+      {new, Updated};
+    true->
+      old
+    end
+  end.
